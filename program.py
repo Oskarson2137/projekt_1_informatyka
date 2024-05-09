31e269880ac45e042d80f81bd6a90b62204e0e2c
325758 <166397716+325758@users.noreply.github.com>
2024-05-09 17:31:32 +0200

    Update program.py

--- program.py
+++ program.py
@@ -155,6 +155,27 @@ class Transformation:
             wyniki.append([x92,y92])
 
     def pl2000(self,fi,lama,m=0.999923):
+       """
+        Funkcja przelicza podane współrzędne geodezyjne krzywoliniowe na współrzędne X, Y na 
+        płaszczyźnie Gaussa-Kruggera, a następnie na współrzędne w układzie 
+        PL 2000.
+
+        Parameters
+        ----------
+        fi : [float] szerokosć geodezyjna danego punktu
+             jednostka: [rad]
+        lama : [float] długosć geodezyjna danego punktu
+              jednostka: [rad]
+        m : TYPE, optional
+            Skala zniekształceń układu PL 2000, wartosć wynosi 0.999923.
+            jednostka: brak
+
+        Returns
+        -------
+        wyniki : [list] lista współrzędnych w układzie PL 2000, w kolejnosci: X2000, Y2000
+                 jednostka: [m]
+
+        """
         wyniki = []
         for fi, lama in zip (fi,lama):
             lama0 = 0
@@ -196,6 +217,22 @@ class Transformation:
         return  wyniki
 
      def Rneu(self, phi, lam):
+        """
+         Funkcja definiuje macierz obrotu układu NEU dla podanej elipsoidy, szerokosci i długosci geodezyjnej
+
+        Parameters
+        ----------
+        phi : [float] szerokosć geodezyjna danego punktu
+             jednostka: [rad]
+        lam : [float] długosć geodezyjna danego punktu
+             jednostka: [rad]
+
+        Returns
+        -------
+        Rneu : [array] macierz obrotu układu NEU
+               jednostka: brak 
+
+        """
            Rneu = np.array([[-np.sin(phi)*np.cos(lam), -np.sin(lam), np.cos(phi)*np.cos(lam)],
                          [-np.sin(phi)*np.sin(lam), np.cos(lam), np.cos(phi)*np.sin(lam)],
                          [np.cos(phi), 0, np.sin(phi)]])
@@ -203,6 +240,31 @@ class Transformation:
         return Rneu
 
       def xyz2neup(self, X, Y, Z, X0, Y0, Z0):
+         """
+          Funkcja przelicza współrzędne geocentryczne XYZ punktu na współrzędne topocentryczne 
+        w układzie NEU ze srodkiem w punkcie, w którym znajduje się nadajnik o współrzędnych X0, Y0, Z0.
+
+        Parameters
+        ----------
+        X : [float] współrzędna geocentryczna X punktu 
+            jednostka: [m]
+        Y : [float] współrzędna geocentryczna Y punktu 
+            jednostka: [m]
+        Z : [float] współrzędna geocentryczna Z punktu 
+            jednostka: [m]
+        X0 : [float] współrzędna geocentryczna X nadajnika 
+            jednostka: [m]
+        Y0 : [float] współrzędna geocentryczna Y nadajnika 
+            jednostka: [m]
+        Z0 : [float] współrzędna geocentryczna Y nadajnika 
+            jednostka: [m]
+
+        Returns
+        -------
+        wyniki : [list] lista współrzędnych punktow układzie topocentrycznym NEU
+                 ze srodkiem w miejscu położenia nadajnika.
+
+        """
          wyniki = []
         p = np.sqrt(X0**2+Y0**2)
         fi = np.arctan(Z0/(p*(1-self.e2)))
@@ -224,6 +286,87 @@ class Transformation:
             wyniki.append(X_rneu.T)
             
         return wyniki
+
+    def odczyt(self,plik_wsadowy, transformacja):
+       """
+        Funkcja przeprowadza na danych z pliku wejsciowego wybraną transformację, następnie
+        tworzy nowy plik z wynikami transformacji o nazwie: 'plik_wynikowy_(nazwa_transformacji)_(args.el).txt'
+
+        Parameters
+        ----------
+        plik_wsadowy : [.txt file] plik z danymi do obliczeń
+            
+        transformacja : [str] zmienna tekstowa, definiująca jaką transformację program przeprowadzi na danych 
+        z pliku wejsciowego. Do wyboru: 'XYZ2BLH' ; 'BLH2XYZ' ; 'PL2000' ; 'PL1992' ; 'XYZ2NEUP'
+
+        Returns
+        -------
+        None.
+
+        """
+        dane = np.genfromtxt(plik_wsadowy,delimiter = " ")
+        if transformacja == 'XYZ2BLH':
+            wyniki = self.hirvonen(dane[:,0], dane[:,1], dane[:,2])
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.10f %0.10f %0.3f')
+        
+        elif transformacja == 'BLH2XYZ':
+            wyniki = self.hirvonen_odw(np.deg2rad((dane[:,0])), np.deg2rad(dane[:,1]), dane[:,2])
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f %0.3f')
+
+        elif transformacja == 'PL2000':
+            wyniki = self.pl2000(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f')
+        
+        elif transformacja == 'PL1992':
+            wyniki = self.pl1992(np.deg2rad(dane[:,0]), np.deg2rad(dane[:,1]))
+            np.savetxt(f"plik_wynikowy_{transformacja}_{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f')
+        
+        elif transformacja == 'XYZ2NEUP':
+            wyniki = self.xyz2neup(dane[1:,0], dane[1:,1], dane[1:,2], dane[0,0], dane[0,1], dane[0,2])
+            np.savetxt(f"plik_wynikowy_{transformacja}._{args.el}.txt", wyniki, delimiter=' ', fmt='%0.3f %0.3f %0.3f')
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-p', type=str, help='Przyjmuje sciezke do pliku z danymi wejsciowymi, jesli plik jest w tym samym folderze co skrypt to wystarczy nazwa pliku z rozszerzeniem')
+    parser.add_argument('-el', type=str, help='Przyjmuje nazwe elipsoidy. Dostepne: WGS84, GRS80 lub KRASOWSKI')
+    parser.add_argument('-t', type=str, help='Przyjmuje nazwe wybranej transformacji. Dostepne: XYZ2BLH, BLH2XYZ, PL2000, PL1992, XYZ2NEUP')
+    args = parser.parse_args()
+
+    elipsoidy = {'WGS84':[6378137.000, 0.00669438002290], 'GRS80':[6378137.000, 0.00669438002290], 'KRASOWSKI':[6378245.000, 0.00669342162296]}
+    transformacje = {'XYZ2BLH': 'XYZ2BLH','BLH2XYZ': 'BLH2XYZ','PL2000':'PL2000','PL1992':'PL1992', 'XYZ2NEUP':'XYZ2NEUP'}
+    
+    wybor = "TAK"
+    try:
+        while wybor =="TAK":
+            if args.el==None:
+                args.el = input(str('Podaj nazwe elipsoidy: '))
+            if args.p==None:
+                args.p = input(str('Wklej sciezke do pliku txt z danymi: '))
+            if args.t==None:
+                args.t = input(str('Jaka transformacje wykonac?: '))
+                    
+            obiekt = Transformation(elipsoidy[args.el.upper()])
+            dane = obiekt.odczyt(args.p, transformacje[args.t.upper()])
+                
+            print('Plik wynikowy zostal utworzony.')
+                
+            wybor = input(str("Jezeli chcesz wykonac kolejna transformacje wpisz TAK jesli chcesz zakonczyc ENTER: ")).upper()
+            args.el = None
+            args.p= None
+            args.t= None
+
+    except FileNotFoundError:
+        print('Podany plik nie istnieje.')
+    except KeyError:
+        print('Zle podana elipsoida lub transformacja.')
+    except IndexError:
+        print('Zly format danych w pliku.')
+    except ValueError:
+        print('Zly format danych w pliku.')
+    finally:
+        print('Koniec programu')
+       
+      
 
    
        
